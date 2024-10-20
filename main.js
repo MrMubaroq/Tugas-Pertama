deffbe481fdb4b9d795953a772218eb9dbf88d77
MrMubaroq <mubaroqahmad89@gmail.com>
2024-10-20 09:39:07 +0800

    commit pertama

---
+++ main.js
@@ -0,0 +1,10 @@
+let angka = prompt("input angka :");
+if (angka <= 1) {
+    console.log("Angka ini adalah  ganjil");
+}else if (angka <= 2 ){
+    console.log("Angka ini adalah  genap");
+}else if (angka <= 3){
+    console.log("Angka ini adalah ganjil");
+}else if (angka <= 4){
+    console.log("Angka ini adalah  genap");
+};
